155417866611f1488fa9922f4761fe91023eacb2
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-14 05:16:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,7 @@ class Application
         resp.write "Your cart is empty."
       else
         @@cart.each do |items|
-          resp.write "#{items}"
+          resp.write "#{items}\n"
         end
       end
     elsif req.path.match(/add/)
