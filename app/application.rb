cf53d7ca9ea9125bf06408983524418e8237b24d
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-14 05:21:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -24,8 +24,8 @@ class Application
       end
     elsif req.path.match(/add/)
       search_item = req.params["item"]
-      if @@items.find do |items|
-        items = search_item
+      if @@items.find do |i|
+        i = search_item
         end
       @@cart << search_item
       resp.write "added #{search_item}"
